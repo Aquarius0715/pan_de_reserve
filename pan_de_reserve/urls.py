c7a2f175f8c5f3483848d4681350eca75bff73ab
ji1wxs <ji1wxs@outlook.jp>
2024-12-09 11:13:53 +0900

    complete loginpage

--- pan_de_reserve/urls.py
+++ pan_de_reserve/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('', views.index, name="index"),
-	path('nyuryoku/', views.nyuryoku, name="nyuryoku"),
-	path('result/', views.result, name="result"),
-    path('ReserveList/',views.ReserveList,name="ReserveList"),
+    path('', views.index, name="index"),
+    path('login/', views.login_view, name="login"),
+    path('nyuryoku/', views.nyuryoku, name="nyuryoku"),
+    path('result/', views.result, name="result"),
+    path('ReserveList/', views.ReserveList, name="ReserveList"),
+    path('adminIndex/', views.admin_index, name="adminIndex"),
 ]
--- pan_de_reserve/views.py
+++ pan_de_reserve/views.py
@@ -1,15 +1,44 @@
-from django.shortcuts import render
-from django.views import View
+from django.shortcuts import render, redirect
+from .forms import LoginForm
+from .models import LoginData
+
 
 def index(request):
     return render(request, 'pan_de_reserve/index.html')
 
+
 def nyuryoku(request):
     return render(request, 'pan_de_reserve/nyuryoku.html')
+
 
 def result(request):
     return render(request, 'pan_de_reserve/result.html')
 
-def ReserveList(request):
-    return render(request,'pan_de_reserve/ReserveList.html')
 
+def ReserveList(request):
+    return render(request, 'pan_de_reserve/ReserveList.html')
+
+
+def login_view(request):
+    error_message = ""
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password"]
+
+            # データベースの認証
+            try:
+                user = LoginData.objects.get(username=username, password=password)
+                return redirect("adminIndex")  # 認証成功 -> メインページへ
+            except LoginData.DoesNotExist:
+                error_message = "ユーザー名またはパスワードが間違っています。"
+
+    else:
+        form = LoginForm()
+
+    return render(request, "pan_de_reserve/login.html", {"form": form, "error_message": error_message})
+
+
+def admin_index(request):
+    return render(request, "pan_de_reserve/adminIndex.html")
