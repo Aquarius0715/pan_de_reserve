25bb76701c57d545916aa7781f5b533303374a07
KEI52110 <uekei41009@gmail.com>
2024-12-13 01:08:38 +0900

    ほぼ完

--- pan_de_reserve/models.py
+++ pan_de_reserve/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import uuid
 
 # Create your models here.
 #$ pip install pillow  # pipenvを利用している場合は pipenv install pillow
@@ -25,6 +26,7 @@ class BakeryItemAllergy(models.Model):
         return f"{self.bakery_item.name} - {self.allergy.name}"
 
 class Reservation(models.Model):
+    id = models.UUIDField(primary_key=True,default=uuid.uuid4 ,editable=False)
     receive_time = models.DateTimeField(verbose_name="受取時間")
     customer_name = models.CharField(max_length=255,verbose_name="名前")
     customer_phone_number = models.CharField(max_length=20,verbose_name="電話番号")
@@ -34,9 +36,10 @@ class Reservation(models.Model):
         return f"Reservation for {self.customer_name} at {self.receive_time}"
 
 class ReservationDetail(models.Model):
+    id = models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
     reservation = models.ForeignKey(Reservation, on_delete=models.CASCADE)
     bakery_item = models.ForeignKey(BakeryItem, on_delete=models.CASCADE)
-    quantity = models.PositiveIntegerField(default=1)
+    quantity = models.DecimalField(max_digits=10, decimal_places=2,verbose_name="個数")
 
     def __str__(self):
         return f"Reservation {self.reservation.id} - {self.bakery_item.name}(x{self.quantity})"
--- pan_de_reserve/views.py
+++ pan_de_reserve/views.py
@@ -14,7 +14,7 @@ def index(request):
         quantities = request.POST.getlist('quantity')
         for i in range(len(pans)):
             print(pans[i]['name'],quantities[i])
-            if quantities[i] != 0:
+            if quantities[i] != '0':
                 order_text += pans[i]['name'] + "×" + quantities[i] + "個\n"
         parms = {
             'order':order_text,
@@ -30,39 +30,88 @@ def index(request):
 
 def nyuryoku(request):
     db = SQLiteManager("db.sqlite3")
-
-    sql_count = "SELECT COUNT(*) FROM pan_de_reserve_reservation"
-    result = db.query(sql_count)
-    total_reservations = result[0][0] if result else 0
-    insert_sql = """INSERT INTO pan_de_reserve_reservation VALUES (?, ?, ?, ?, ?)"""
+    insert_reservation_sql = """INSERT INTO pan_de_reserve_reservation VALUES (?, ?, ?, ?, ?)"""
+    insert_reservation_detail_sql = """INSERT INTO pan_de_reserve_reservationdetail VALUES (?, ?, ?, ?)"""
     message = ""
     
     if (request.method == 'POST'):
-        print(request.POST)
-        try:
-            params = (
-            total_reservations,
-            request.POST['receive_time'],  # 受取時間
-            request.POST['customer_name'],             # 顧客名
-            request.POST['customer_phone_number'],        # 電話番号
-            0                  # 受取状況
+        this_uuid = uuid.uuid4()
+        print(this_uuid)
+        orders = request.POST['order'].split()
+        pans = []
+        quantities = []
+        for order in orders:
+            tmp = order.split("×")
+            num = int(str(tmp[1])[0:-1])
+            if num != 0:
+                pans.append(tmp[0])
+                quantities.append(num)
+        pan_sql = "SELECT id FROM pan_de_reserve_bakeryitem WHERE name = ?"
+        pan_ids = []
+        for pan in pans:
+            pan_name_params = (pan,
             )
-            rows_affected = db.execute(insert_sql, params)
+            results = db.query(pan_sql,pan_name_params)
+            pan_ids.extend([re[0] for re in results])
+
+        params = (
+        request.POST['receive_time'],  # 受取時間
+        request.POST['customer_name'],             # 顧客名
+        request.POST['customer_phone_number'],        # 電話番号
+        0,                  # 受取状況
+        str(this_uuid).replace('-',''),
+        )
+        try:
+            rows_affected = db.execute(insert_reservation_sql, params)
             print(f"新しい予約を {rows_affected} 件追加しました")
             if rows_affected == -1:
+                print("rows affected = ",rows_affected)
+                print("ERROR!!")
                 print(request.POST)
+                message = "正しく再入力してください"
             else:
-                return render(request, 'pan_de_reserve/result.html')
-        except:
-            print("ERROR!!")
+                pass
+                # return render(request, 'pan_de_reserve/result.html')
+        except Exception as e:
+            print(f"予期せぬエラー:{e}")
+            print("ERROR number 1!")
             print(request.POST)
             message = "正しく再入力してください"
+        for obj in Reservation.objects.all():
+            print(obj)
+        try:
+            flag = -1
+            for i in range(len(pan_ids)):
+                flag = 0
+                new_uuid = uuid.uuid4()
+                params = (
+                    pan_ids[i],
+                    str(this_uuid).replace('-',''),
+                    quantities[i],
+                    str(new_uuid).replace('-',''),
+                )
+                rows_affected = db.execute(insert_reservation_detail_sql,params)
+                print(f"新しい予約詳細を {rows_affected} 件追加しました")
+                if rows_affected == -1:
+                    print("ERROR!!")
+                    print(request.POST)
+                    message = "正しく再入力してください"
+                    break
+                else:
+                    flag = 1
+            if flag == 1:
+                return render(request, 'pan_de_reserve/result.html')
+        except:
+            print("ERROR number 2!!")
+            print(request.POST)
+            message = "正しく再入力してください"            
 
 
     modelform_dict = {
         "msg":message,
         "form1": ReservationAdd(),
         "form2": ReservationDetailAdd(),
+        "pans":request.POST['order'],
     }
 
     return render(request, 'pan_de_reserve/nyuryoku.html',modelform_dict)
